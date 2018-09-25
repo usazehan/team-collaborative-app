d3e236ddcbf24f9460415d1e4dd6b6b6dee60648
Zehan Sunesara <usazehan@gmail.com>
2018-09-24 21:14:48 -0500

    Display message

--- src/components/messages/Messages.vue
+++ src/components/messages/Messages.vue
@@ -3,8 +3,14 @@
         <div class="messages__content">
             <h2 class="ui inverted center aligned header"># Channel name</h2>
             <div class="ui segment">
-                <div class="uicomments">
-
+                <div class="ui comments">
+                    <transition-group tag="div" name="list">
+                        <single-message 
+                            :message="message" 
+                            v-for="message in messages" 
+                            :key="message.id"
+                        ></single-message>
+                    </transition-group>
                 </div>
             </div>
         </div>
@@ -15,12 +21,46 @@
 <script>
 
     import MessageForm from './MessageFrom'
+    import {mapGetters} from 'vuex'
+    import SingleMessage from './SingleMessage'
 
     export default {
         name: 'messages',
-        components: {MessageForm},
+        components: {MessageForm, SingleMessage},
         data() {
-            messagesRef: firebase.database().ref('messages')
+            return {
+                messagesRef: firebase.database().ref('messages'),
+                messages: [],
+                channel: null  
+            }
+        },
+        computed: {
+            ...mapGetters(['currentChannel', 'currentUser'])
+        },
+        watch: {
+            currentChannel() {
+                this.messages = []
+                this.detachListeners()
+                this.addlisteners()
+                this.channel = this.currentChannel
+            }
+        },
+        methods: {
+            addlisteners() {
+                this.messagesRef.child(this.currentChannel.id).on('child_added', snap => {
+                    let message = snap.val()
+                    message['id'] = snap.key
+                    this.messages.push(message)
+                })
+            },
+            detachListeners() {
+                if(this.channel !== null) {
+                    this.messagesRef.child(this.channel.id).off('child_added')
+                }
+            }
+        },
+        beforeDestroy() {
+            this.detachListeners()
         }
     }
 </script>
@@ -35,5 +75,12 @@
     }
     .messages__constainer .comments{
         font-size: 1.2em;
+    }
+    .list-enter-active {
+        transition: all 0.3s;
+    }
+    .list-enter, .list-leave-to{
+        opacity: 0;
+        transform: translateX(30px);
     }
 </style>
---
+++ src/components/messages/SingleMessage.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="comment comment_container">
+        <a class="avatar">
+            <img src="message.user.avatar">
+        </a>
+        <div class="content" :class="{'comment__self': selfMessage(message.user)}" >
+            <a class="author">{{message.user.name}}</a>
+            <div class="metadata">
+                <span class="date comment__date">{{message.timestamp | fromNow}}</span>
+            </div>
+            <div class="text">{{message.content}}</div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+    import {mapGetters} from 'vuex'
+    import moment from 'moment'
+
+    export default {
+        name: 'single-message',
+        props: ['message'],
+        computed: {
+                ...mapGetters(['currentUser'])
+        },
+        methods: {
+            selfMessage(user) {
+                return user.id === this.currentUser.uid
+            }
+        },
+        filters: {
+            fromNow(value) {
+                return moment(value).fromNow()
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
+
